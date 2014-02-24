f4446de0859df1b3eadbf4c9d86000f1c4bdf96a
raphaelstary <raphael.stary@gmail.com>
2014-02-24 14:08:40 +0100

    code comments

--- www/js/main.js
+++ www/js/main.js
@@ -20,6 +20,7 @@ require(['require', 'lib/domReady', 'App', 'GameLoop', 'render/Renderer', 'input
     var App = require('App'),
         GameLoop = require('GameLoop'),
         Renderer = require('render/Renderer'),
+//        ### old tilt controller 1/2
 //        OrientationHandler = require('input/OrientationHandler'),
         HitDetector = require('HitDetector'),
         FullScreenController = require('input/FullScreenController'),
@@ -51,24 +52,29 @@ require(['require', 'lib/domReady', 'App', 'GameLoop', 'render/Renderer', 'input
     var fsCtrl = new FullScreenController(screen);
     var listener = function () {
         screen.removeEventListener('click', listener);
+//        ### full screen controller 1/1
 //        fsCtrl.request();
-//        document.documentElement.mozRequestFullScreen();
         app.startGame();
 
+//        #### tilt controls 1/2
 //        screen.addEventListener('touchend', app.fire.bind(app));
 
-        screen.addEventListener('touchstart', dPad.touchStart.bind(dPad));
-        screen.addEventListener('touchmove', dPad.touchMove.bind(dPad));
-        screen.addEventListener('touchend', dPad.touchEnd.bind(dPad));
+//        #### dpad controls 1/1
+//        screen.addEventListener('touchstart', dPad.touchStart.bind(dPad));
+//        screen.addEventListener('touchmove', dPad.touchMove.bind(dPad));
+//        screen.addEventListener('touchend', dPad.touchEnd.bind(dPad));
 
     };
     screen.addEventListener('click', listener);
     var oracle = new BrowserOracle(navigator.userAgent);
     oracle.init();
+
+//    ### old tilt controller 2/2
 //    var orientationHandler = new OrientationHandler(app, INNER_WIDTH, INNER_HEIGHT, oracle.isFirefox);
 //    resizeBus.push(orientationHandler.resize.bind(orientationHandler));
-
 //    window.addEventListener('deviceorientation', orientationHandler.handle.bind(orientationHandler));
+
+//    ### tilt controls 2/2
 //    var tiltPad = new TiltController(app, oracle.isFirefox);
 //    window.addEventListener('deviceorientation', tiltPad.handleOrientation.bind(tiltPad));
 
